2342d78adb7f35720d4396a2e6c2975a4465d27a
Sergio Gabalan <segabalanv@unal.edu.co>
2016-04-21 12:08:42 -0500

    Initial version of start screen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
