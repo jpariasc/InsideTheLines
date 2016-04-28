1e7a209ffe7a3409b4580a815a7483ef2ebb8b41
Sergio Gabalan <segabalanv@unal.edu.co>
2016-04-28 15:55:12 -0500

    Added basic character model and basic camera and movement scripts

---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitCamera : MonoBehaviour {
+
+    public float rotSpeed = 1.5f;
+
+    [SerializeField] private Transform target;
+    private float _rotY;
+    private Vector3 _offset;
+
+	void Start () {
+        _rotY = transform.eulerAngles.y;
+        _offset = target.position - transform.position;	
+	}
+
+    void LateUpdate() {
+        float horInput = Input.GetAxis ("Horizontal");
+        if (horInput != 0) {
+            _rotY += horInput * rotSpeed;
+        } else {
+            _rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
+        }
+        Quaternion rotation = Quaternion.Euler (0, _rotY, 0);
+        transform.position = target.position - (rotation * _offset);
+        transform.LookAt (target);
+    }
+}
---
+++ Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+public class RelativeMovement : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+    private CharacterController _charController;
+
+    public float rotSpeed = 15.0f;
+    public float movSpeed = 6.0f;
+
+    void Start () {
+        _charController = GetComponent<CharacterController> ();
+    }
+
+    void Update() {
+        Vector3 movement = Vector3.zero;
+        float horInput = Input.GetAxis ("Horizontal");
+        float vertInput = Input.GetAxis ("Vertical");
+        if (horInput != 0 || vertInput != 0) {
+            movement.x = horInput * movSpeed;
+            movement.z = vertInput * movSpeed;
+            movement = Vector3.ClampMagnitude (movement, movSpeed);
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3 (0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection (movement);
+            target.rotation = tmp;
+            Quaternion direction = Quaternion.LookRotation (movement);
+            transform.rotation = Quaternion.Lerp (transform.rotation, direction, rotSpeed * Time.deltaTime);
+        }
+        movement *= Time.deltaTime;
+        _charController.Move (movement);
+    }
+
+}
